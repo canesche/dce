2b3b36c98334db65626e3989737a91699cc1cfb8
canesche <michael.canesche@gmail.com>
2021-07-24 08:49:18 -0300

    update: run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 set -e
 
+# CONFIG WHERE YOUR LLVM INSTALL, REQUIRE VERSION COMPILE FROM GIT
 LLVM_INSTALL_DIR="/home/canesche/git/llvm-project/build/" # </path/to/llvm/>, if you use binary installation, you can leave empty this string 
-LLVM_OPT="/home/canesche/git/llvm-project/build/bin/opt" # </path/to/opt>
-CLANG="/home/canesche/git/llvm-project/build/bin/clang" # </path/to/clang>
+
+
+#ADDING BENCHMARKS WITH SPACE BETWEEN WORDS
+BENCH=(
+	test1 test2 test3 test4 test5 test6 test7 
+	test8
+)
+
+# DON'T NEED CHANGE BELOW IF YOU COMPILE FROM GIT
+LLVM_OPT=$LLVM_INSTALL_DIR"/bin/opt" # </path/to/opt>
+CLANG=$LLVM_INSTALL_DIR"/bin/clang" # </path/to/clang>
 
 # PATH LIBRARY
 PATH_LIB="build/lib/libRangeAnalysis.so"
@@ -19,24 +29,29 @@ cd ..
 echo ""
 echo "Benchmarks"
 
-EXAMPLE=benchmarks/test8
+for ((i = 0; i < ${#BENCH[@]}; i++)); do
 
-mkdir -p "results/test2"
+	EXAMPLE=benchmarks/${BENCH[i]}
+	DIR_RESULT="results/"${BENCH[i]}
 
-echo "Executing the pass for bench: "$EXAMPLE
-# analysis pass
-$CLANG -fno-discard-value-names -Xclang -disable-O0-optnone -c -emit-llvm $EXAMPLE".c" -o $EXAMPLE".bc"
-$LLVM_OPT -instnamer -mem2reg -break-crit-edges $EXAMPLE".bc" -o $EXAMPLE".ll"
-   
-$LLVM_OPT -load $PATH_LIB -vssa $EXAMPLE".ll" -o $EXAMPLE".vssa.ll"
-$LLVM_OPT -stats -load $PATH_LIB -dead-code-elimination $EXAMPLE".vssa.ll" -S -o=$EXAMPLE".sol.ll"
+	mkdir -p $DIR_RESULT
 
-$LLVM_OPT -dot-cfg $EXAMPLE".vssa.ll" -disable-output
-dot -Tpdf -o function_antes.pdf .foo.dot 
-
-$LLVM_OPT -dot-cfg $EXAMPLE".sol.ll" -disable-output
-dot -Tpdf -o function_depois.pdf .foo.dot 
+	echo "Executing the pass for bench: "$EXAMPLE
+	# analysis pass
+	$CLANG -fno-discard-value-names -Xclang -disable-O0-optnone -c -emit-llvm $EXAMPLE".c" -o $EXAMPLE".bc"
+	$LLVM_OPT -instnamer -mem2reg -break-crit-edges $EXAMPLE".bc" -o $EXAMPLE".ll"
+	   
+	$LLVM_OPT -load $PATH_LIB -vssa $EXAMPLE".ll" -o $EXAMPLE".vssa.ll"
+	$LLVM_OPT -stats -load $PATH_LIB -dead-code-elimination $EXAMPLE".vssa.ll" -S -o=$EXAMPLE".sol.ll"
+	
+	# generate dot-cfg before
+	$LLVM_OPT -dot-cfg $EXAMPLE".vssa.ll" -disable-output
+	dot -Tpdf .foo.dot -o $DIR_RESULT/foo_antes.pdf 
+	
+	# generate dot-cfg after
+	$LLVM_OPT -dot-cfg $EXAMPLE".sol.ll" -disable-output
+	dot -Tpdf .foo.dot -o $DIR_RESULT/foo_depois.pdf 
+done
 
 echo "clean .ll and .bc"
-rm benchmarks/*.ll
-rm benchmarks/*.bc
+rm benchmarks/*.ll benchmarks/*.bc benchmarks/*.rbc
